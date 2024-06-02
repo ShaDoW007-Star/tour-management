cf6ed82aa5ace126fc0e323349b38c727a1ddf17
ShaDoW007-Star <patelaryan460@gmail.com>
2024-06-02 12:14:16 +0530

    comment

--- contact.php
+++ contact.php
@@ -51,7 +51,6 @@ if(isset($_POST['send'])){
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
-   <!-- custom css file link  -->
    <link rel="stylesheet" href="css/style.css">
 
 </head>
